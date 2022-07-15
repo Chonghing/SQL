a1c43652be666a08fda37aec6338c490d7b28ee0
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:33:01 +0100

    Delete Victime Ethnicity.sql

--- Victime Ethnicity.sql
+++
@@ -1,4 +0,0 @@
-select Officer_Observed_Ethnicity as 'Victim Ethnicity', count (*) as Number, count(*) * 100.0 / sum(count(*)) over() as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Officer_Observed_Ethnicity
-order by percentage desc
