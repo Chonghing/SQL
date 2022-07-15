ca5e1560345b67ca5a0c88d4b8180ae9e76a318e
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:32:54 +0100

    Delete Victime Domestic.sql

--- Victime Domestic.sql
+++
@@ -1,4 +0,0 @@
-select Domestic_Abuse as 'Domestic Abuse', count (*) as Number, count(*) * 100.0 / sum(count(*)) over() as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Domestic_Abuse
-order by percentage desc
