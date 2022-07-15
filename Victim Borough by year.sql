4ac8d942eb9ecc94096c7a7782039d530b2c7a37
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:31:33 +0100

    Delete Victim Borough by year.sql

--- Victim Borough by year.sql
+++
@@ -1,4 +0,0 @@
-select RIGHT(Recorded_Date, 2) as Year, Borough, count (*) as Number, count(*) * 100.0 / sum(count(*)) over(partition by RIGHT(Recorded_Date, 2)) as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Borough, RIGHT(Recorded_Date, 2) 
-order by Borough, Year
