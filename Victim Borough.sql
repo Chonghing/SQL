df8b0f1c7d62b7b1fa54150535aa6b4b45bc91cc
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:31:49 +0100

    Delete Victim Borough.sql

--- Victim Borough.sql
+++
@@ -1,4 +0,0 @@
-select Borough as 'Borough', count (*) as Number, count(*) * 100.0 / sum(count(*)) over() as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Borough
-order by percentage desc
