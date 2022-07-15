2da85850a3e65d5583775daa2844b4db6e6ba8ca
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:30:22 +0100

    Delete Create Borough Rank table.sql

--- Create Borough Rank table.sql
+++
@@ -1,7 +0,0 @@
-select v.year, p.name as Borough, p.population as Population, p.percentage as 'Population percentage', v.number as 'Victim number', v.percentage as 'Victim percentage', 
-p.percentage-v.percentage as Difference, rank () over (partition by v.year order by p.percentage-v.percentage desc) rank
-into Borough_rank
-from borough_population as p
-left join borogh_victim_year as v
-on p.name = v.borough
-order by p.population desc, p.name, v.year
