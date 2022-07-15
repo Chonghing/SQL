fca9726623eae695a8ac29c189770c3e31c0c147
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:32:06 +0100

    Delete Victim Ethnicity by year.sql

--- Victim Ethnicity by year.sql
+++
@@ -1,4 +0,0 @@
-select RIGHT(Recorded_Date, 2) as Year, Officer_Observed_Ethnicity as 'Victim Ethnicity', count (*) as Number, count(*) * 100.0 / sum(count(*)) over(partition by RIGHT(Recorded_Date, 2)) as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Officer_Observed_Ethnicity, RIGHT(Recorded_Date, 2) 
-order by Officer_Observed_Ethnicity, Year
