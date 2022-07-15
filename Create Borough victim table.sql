42256574b1d4dc3a5b205161244098a408d46dbf
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:30:36 +0100

    Delete Create Borough victim table.sql

--- Create Borough victim table.sql
+++
@@ -1,5 +0,0 @@
-select RIGHT(Recorded_Date, 2) as Year, Borough, count (*) as Number, count(*) * 100.0 / sum(count(*)) over(partition by RIGHT(Recorded_Date, 2)) as Percentage
-into Borogh_victim_year
-from [LDS Homicide Victims 2003-2021]
-group by Borough, RIGHT(Recorded_Date, 2) 
-order by Borough, Year
