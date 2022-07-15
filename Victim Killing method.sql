82a92970c4a910c8625acd4996637050a5e01185
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:32:19 +0100

    Delete Victim Killing method.sql

--- Victim Killing method.sql
+++
@@ -1,4 +0,0 @@
-select Method_of_Killing as 'Killing Method', count (*) as Number, count(*) * 100.0 / sum(count(*)) over() as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Method_of_Killing
-order by percentage desc
