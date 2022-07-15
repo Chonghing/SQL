a88540ca703582dd5c491b3fa49c9406ff6bfb42
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:32:12 +0100

    Delete Victim Killing method by year.sql

--- Victim Killing method by year.sql
+++
@@ -1,4 +0,0 @@
-select RIGHT(Recorded_Date, 2) as Year, Method_of_Killing as 'Killing Method', count (*) as Number, count(*) * 100.0 / sum(count(*)) over(partition by RIGHT(Recorded_Date, 2)) as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Method_of_Killing, RIGHT(Recorded_Date, 2) 
-order by Method_of_Killing, Year
