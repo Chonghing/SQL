5947793830f24d5f4c2ef09bf6b169be7f29bb07
Chonghing <109066799+Chonghing@users.noreply.github.com>
2022-07-15 20:32:25 +0100

    Delete Victim Sex by year.sql

--- Victim Sex by year.sql
+++
@@ -1,4 +0,0 @@
-select RIGHT(Recorded_Date, 2) as Year, Sex, count (*) as Number, count(*) * 100.0 / sum(count(*)) over(partition by RIGHT(Recorded_Date, 2)) as Percentage
-from [LDS Homicide Victims 2003-2021]
-group by Sex, RIGHT(Recorded_Date, 2) 
-order by Sex, Year
